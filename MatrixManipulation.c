86d690ca2ff448d687be1fc6a6fdd48c0d18ed8d
finley-webber <86814431+finley-webber@users.noreply.github.com>
2023-09-29 16:42:47 +1000

    Add files via upload

--- MatrixManipulation.c
+++ MatrixManipulation.c
@@ -46,6 +46,6 @@ int main(int argc, char** argv) {
     printf("%i elements long\n", num);
     printf("-----------------------------------------------------------------\n");
     free(arr);
-
+    
     return 0;
 }
